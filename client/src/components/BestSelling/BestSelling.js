da5090398882d981c6069462fff37ba8dce43738
avinasdube <avidubey712@gamil.com>
2023-12-07 00:50:18 +0530

    [update] : updated image imports

--- client/src/components/BestSelling/BestSelling.js
+++ client/src/components/BestSelling/BestSelling.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './BestSelling.scss';
 
 // IMPORTING ASSETS
-import p1 from '../../assets/images/p1.jpg';
-import p2 from '../../assets/images/p2.jpg';
+import checkWhite from '../../assets/images/casual-shirt/wc1.jfif';
+import redPrint from '../../assets/images/casual-shirt/rs2.jfif';
 
 const BestSelling = () => {
     return (
@@ -11,17 +11,17 @@ const BestSelling = () => {
             <div className="bestProduct">
                 <div className="content">
                     <div className="top">Trending</div>
-                    <div className="middle">GANGSTA STYLE CHECK SHIRT</div>
+                    <div className="middle">RELAXED FIT FLANNEL SHIRT</div>
                     <div className="bottom"></div>
                 </div>
-                <img src={p1} alt=''></img>
+                <img src={checkWhite} alt=''></img>
             </div>
             <div className="bestProduct">
                 <div className="content">
                     <div className="top">Trending</div>
-                    <div className="middle">ELITE CLASS LUXURY WHITE SHIRT</div>
+                    <div className="middle">PURE COTTON RED PRINTED SHIRT</div>
                 </div>
-                <img src={p2} alt=''></img>
+                <img src={redPrint} alt=''></img>
             </div>
         </div>
     )
