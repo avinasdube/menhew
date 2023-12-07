3daf79c307331a573ed9b3717cbada8a51b38ba6
avinasdube <avidubey712@gamil.com>
2023-12-07 13:21:47 +0530

    [update] : updated best selling

--- client/src/components/BestSelling/BestSelling.js
+++ client/src/components/BestSelling/BestSelling.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './BestSelling.scss';
 
 // IMPORTING ASSETS
-import checkWhite from '../../assets/images/casual-shirt/wc1.jfif';
-import redPrint from '../../assets/images/casual-shirt/rs2.jfif';
+import flannelShirt from '../../assets/images/casual-shirt/regular_fit_flannel_shirt.jfif';
+import twillJogger from '../../assets/images/trousers/regular_fit_twill_joggers3.jfif';
 
 const BestSelling = () => {
     return (
@@ -11,17 +11,17 @@ const BestSelling = () => {
             <div className="bestProduct">
                 <div className="content">
                     <div className="top">Trending</div>
-                    <div className="middle">RELAXED FIT FLANNEL SHIRT</div>
+                    <div className="middle">REGULAR FIT FLANNEL SHIRT</div>
                     <div className="bottom"></div>
                 </div>
-                <img src={checkWhite} alt=''></img>
+                <img src={flannelShirt} alt=''></img>
             </div>
             <div className="bestProduct">
                 <div className="content">
                     <div className="top">Trending</div>
-                    <div className="middle">PURE COTTON RED PRINTED SHIRT</div>
+                    <div className="middle">REGULAR FIT TWILL JOGGER</div>
                 </div>
-                <img src={redPrint} alt=''></img>
+                <img src={twillJogger} alt=''></img>
             </div>
         </div>
     )
