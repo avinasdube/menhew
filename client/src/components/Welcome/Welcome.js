51c681bf5320cbdd399871b65f8ce470c7c76613
avinasdube <avidubey712@gamil.com>
2023-12-08 01:24:33 +0530

    [button] : added a new redirect button

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Welcome.scss';
 
+import { Link } from 'react-router-dom';
+import upright from '../../assets/icons/upright.png';
+
 const Welcome = () => {
   return (
     <div className="welcomeContainer">
-      <header className="header">A gentleman is never off-fashion.</header>
+      <header className="header">A gentleman is never off-fashion</header>
       <section className="subheader">Huge collection of outfits, curated just for you.</section>
-      
+      <div className="redirect">
+        <Link to="/categories" className='link'><img src={upright} alt=''></img></Link>
+      </div>
     </div>
   )
 }
