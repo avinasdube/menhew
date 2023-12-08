0ecf2783aba849023628ccdbae08ccba2d5c53d5
avinasdube <avidubey712@gamil.com>
2023-12-08 20:38:38 +0530

    [modified] : separated header, welcome and imported new reusable components

--- client/src/pages/Categories/Categories.jsx
+++ client/src/pages/Categories/Categories.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './Categories.scss';
 
 // IMPORTING COMPONENTS
-import Navbar from '../../components/Navbar/Navbar';
 import CatBox from '../../components/CatBox/CatBox';
+import Heading from '../../components/Heading/Heading';
+import Header from '../../components/Header/Header';
+import Welcome from '../../components/Welcome/Welcome';
 
 // IMPORTING ASSETS
 import shirtImg from '../../assets/images/casual-shirt/regular_fit_denim_shirt.jfif';
 import jeanImg from '../../assets/images/jeans/slim_cargo_jeans4.jfif';
 import trouserImg from '../../assets/images/trousers/cargo_joggers4.jfif';
 import tshirtImg from '../../assets/images/tshirts/slim_fit_white_tshirt.jfif';
-import Heading from '../../components/Heading/Heading';
+import catWelcome from '../../assets/images/catwelcome.jpg'
 
 
 const Categories = () => {
@@ -40,13 +42,12 @@ const Categories = () => {
 
   return (
     <div className="categoriesContainer" data-testid="categories">
-      <header className="header">
-        <Navbar />
-        <div className="headerIntro">
-          <div className="introHeading">Unleash your fashion experience</div>
-          <div className="introSubheading">Choose from our vast collection of outfits. Explore menhew collection.</div>
-        </div>
-      </header>
+      <Header bgImg={catWelcome}>
+        <Welcome
+          wHeader={"Unleash your fashion experience"}
+          wSubHeader={"Choose from our vast collection of outfits. Explore menhew collection"}/>
+      </Header>
+
       <main className="catMain">
         <Heading>CATEGORIZED FOR YOU</Heading>
         <div className="catGrid">
