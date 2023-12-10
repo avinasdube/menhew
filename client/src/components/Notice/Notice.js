37f4f21b1587238181050923eb8efdd156e42154
avinasdube <avidubey712@gamil.com>
2023-12-10 16:04:00 +0530

    [new] : created new notice component

---
+++ client/src/components/Notice/Notice.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Notice.scss';
+
+const Notice = () => {
+    return (
+        <div className="noticeContainer">
+            <div className="noticeBox">
+                Notice : This it to notify all users/viewers that this project/website is under development, 
+                being handcrafted by <a href="https://github.com/avinasdube">Avinash Dubey</a>
+            </div>
+        </div>
+    )
+}
+
+export default Notice;
