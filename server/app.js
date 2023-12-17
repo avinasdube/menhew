f482860007e364ecd35073de22d6c05823c59048
avinasdube <avidubey712@gamil.com>
2023-12-17 17:00:53 +0530

    [config] : configured server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import connectDB from './config/dbconfig.js';
 
+import shirtsRoute from './routes/shirtsRoute.js';
 
 dotenv.config();
 
@@ -10,24 +11,26 @@ const app = express()
 const PORT = process.env.PORT;
 
 // middleware
-const corsOptions = {    origin: ["https://menhew.onrender.com", "http://localhost:3000"]// frontend URI (ReactJS)
-
+const corsOptions = {
+    origin: ["https://menhew.onrender.com", "http://localhost:3000"] // frontend URI (ReactJS)
 }
 
 app.use(cors(corsOptions));
 app.use(express.json())
 
-// connect MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const PORT = process.env.PORT || 8000
-    app.listen(PORT, () => {
-        console.log(`App is Listening on PORT ${PORT}`);
-    })
-}).catch(err => {
-    console.log(err);
-});
-
 // route
+app.use("/api", shirtsRoute)
 app.get("/", (req, res) => {
-    res.status(201).json({message: "Connected to Backend!"});
-});
+    res.status(201).json({ message: "yess" });
+})
+
+// start server if database is connected
+connectDB()
+    .then(() => {
+        app.listen(PORT || 8800, () => {
+            console.log(`Server is running at port : ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("Error starting the server !!", error)
+    })
