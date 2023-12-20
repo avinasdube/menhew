040f8be79cbd7b2e7455dd04bbce1c322e831db0
avinasdube <avidubey712@gamil.com>
2023-12-20 17:00:24 +0530

    [modified] : modified form submission logic

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import Heading from '../../components/Heading/Heading';
 import NavButton from '../../components/NavButton/NavButton';
+import { login } from '../../api/api';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   // useState hook to set user input
   const [inputs, setInputs] = useState({
     email: "",
@@ -18,25 +22,31 @@ const Login = () => {
   }
 
   // function to handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(inputs)
+    try {
+      const response = await login(inputs);
+      console.log(response);
+      navigate('/home');
+    } catch (error) {
+      console.error("Unexpected Error !", error);
+    }
   }
 
   return (
     <div className="authContainer" data-testid="login">
       <div className="authBox">
         <Heading>Login to menhew</Heading>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="inputBox">
             <label className="inputLabel" htmlFor="emailId">Enter your email : </label>
-            <input className="inputField" type="email" name="email" id="emailId" onChange={handleChange} key="email"></input>
+            <input className="inputField" type="email" name="email" id="emailId" onChange={handleChange} key="email" required></input>
           </div>
           <div className="inputBox">
             <label className="inputLabel" htmlFor="passId">Enter your password : </label>
-            <input className="inputField" type="password" name="password" id="passId" onChange={handleChange} key="password"></input>
+            <input className="inputField" type="password" name="password" id="passId" onChange={handleChange} key="password" required></input>
           </div>
-          <button className="authButton" onClick={handleSubmit}>Login</button>
+          <button type="submit" className="authButton">Login</button>
           <div className="already">
             Not signed up ? <NavButton fweight={600} fcolor={'#5C8331'} link={'/signup'}>Signup</NavButton> now !
           </div>
--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import './Signup.scss';
 import Heading from '../../components/Heading/Heading';
 import NavButton from '../../components/NavButton/NavButton';
-import { signUp } from '../../api/api';
+import { signin } from '../../api/api';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-
-  const [uttar, setUttar] = useState();
+  const navigate = useNavigate();
 
   // useState hook to set user input
   const [inputs, setInputs] = useState({
-    name: "",
+    fullname: "",
     email: "",
     password: "",
   })
@@ -26,54 +26,33 @@ const Signup = () => {
   // function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // try {
-    //   const { response } = await signUp(inputs)
-    //   setUttar(response);
-    //   console.log(uttar);
-
-    // } catch (error) {
-    //   console.error("Unexpected Error !", error);
-    // }
-
-    const { loading, error, status, promise } = signUp(inputs);
-
-    // Check loading state
-    if (loading) {
-      console.log('Loading...');
-      return;
+    try {
+      const response = await signin(inputs);
+      console.log(response);
+      navigate('/login');
+    } catch (error) {
+      console.error("Unexpected Error !", error);
     }
-
-    // Check for errors
-    if (error) {
-      console.error('Error:', error);
-      console.log('Status:', status);
-      return;
-    }
-
-    // If successful, you can access the response data
-    promise.then(({ response }) => {
-      console.log('Response:', response);
-    });
   }
 
   return (
     <div className="authContainer" data-testid="signup">
       <div className="authBox">
         <Heading>Signup to menhew</Heading>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="inputBox">
             <label className="inputLabel" htmlFor="nameId">Enter your name : </label>
-            <input className="inputField" type="text" id="nameId" name="name" onChange={handleChange}></input>
+            <input className="inputField" type="text" id="nameId" name="fullname" onChange={handleChange} required></input>
           </div>
           <div className="inputBox">
             <label className="inputLabel" htmlFor="emailId">Enter your email : </label>
-            <input className="inputField" type="email" id="emailId" name="email" onChange={handleChange}></input>
+            <input className="inputField" type="email" id="emailId" name="email" onChange={handleChange} required></input>
           </div>
           <div className="inputBox">
             <label className="inputLabel" htmlFor="passId">Enter your password : </label>
-            <input className="inputField" type="password" id="passId" name="password" onChange={handleChange}></input>
+            <input className="inputField" type="password" id="passId" name="password" onChange={handleChange} required></input>
           </div>
-          <button className="authButton" onClick={handleSubmit}>Signup</button>
+          <button type="submit" className="authButton">Signup</button>
           <div className="already">
             Already signed up ? <NavButton fweight={600} fcolor={'#5C8331'} link={'/login'}>Login</NavButton> now !
           </div>
