305f7780e688a011298325f124bfc7d6ffa5dd42
avinasdube <avidubey712@gamil.com>
2023-12-20 17:25:17 +0530

    [modified] : modified signup controller logic and added login controller logic

--- server/controllers/authControl.js
+++ server/controllers/authControl.js
@@ -1,10 +1,45 @@
+import { User } from "../models/userModel.js";
 
 export const signup = async (req, res) => {
+    const { fullname, email, password } = req.body;
+
     try {
-        const { name, email, password } = await req.body;
-        res.json({ message: "hi from the signup controller" })
-        console.log(name, email, password);
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            const newUser = new User({ fullname, email, password });
+            await newUser.save();
+            res.status(201).json({ message: 'User registered successfully' });
+        } else {
+            res.status(400).json({ error: 'Email already exists.' })
+        }
+
     } catch (error) {
-        console.error("Error getting data : ", error);
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Email Not Found' })
+        }
+
+        const passwordMatch = await user.comparePassword(password);
+
+        if (passwordMatch) {
+            res.status(200).json({ message: 'Login Successful' });
+        } else {
+            res.status(401).json({ error: "Password Doesn't Matches" })
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' })
     }
 }
