929a35abf7e2a060302953d1273683b4cbb41f82
avinasdube <avidubey712@gamil.com>
2023-12-20 17:57:02 +0530

    [updated] : updated server-api url

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,7 @@
 // necessary constants
 export const API_BASE_URL = process.env.NODE_ENV === 'development'
     ? "http://localhost:4000/api"
-    : "https://menhew-server.onrender.com/";
+    : "https://menhew-server.onrender.com/api";
 
 export const API_ENDPOINTS = {
     signup: "/auth/signup",
