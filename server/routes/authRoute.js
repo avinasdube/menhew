f29cc218b00271762e1877e21d498d0503feca14
avinasdube <avidubey712@gamil.com>
2023-12-26 17:28:09 +0530

    [modified] : added new route for logout

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { login, signup } from "../controllers/authControl.js";
+import { login, logout, signup } from "../controllers/authControl.js";
+import { verifyJWT } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+// secured routes
+router.get("/logout", verifyJWT, logout);
 
 export default router;
