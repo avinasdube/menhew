6f227f24bb50028a6240fee1358ca584bdc18dcd
avinasdube <avidubey712@gamil.com>
2023-12-27 17:20:23 +0530

    [modified] : modified logout and navbutton implementation

--- client/src/components/ProfileBox/ProfileBox.js
+++ client/src/components/ProfileBox/ProfileBox.js
@@ -5,26 +5,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { userLogout } from '../../reducers/authSlice';
 import { logout } from '../../api/api';
 import NavButton from '../NavButton/NavButton';
+import { useNavigate } from 'react-router-dom';
 
 const ProfileBox = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const { user } = useSelector((state) => state.auth.currentUser);
+    const currentUser = useSelector((state) => state.auth.currentUser);
 
     const handleLogout = async () => {
         await logout();
-        dispatch(userLogout(null))
+        dispatch(userLogout())
+        navigate('/login');
     }
 
     return (
         <div className="profileBoxContainer">
             <div className="profileBoxContent">
-                <NavButton
-                    children={`${user.fullname}`}
+                {currentUser ? <NavButton
+                    children={`${currentUser?.user?.fullname}`}
                     fweight={600}
                     fsize={'10px'}
                     fcolor={'green'}
-                    link={`/profile/:${user.fullname}`} />
+                    link={`/profile/:${currentUser?.user.fullname}`} /> : ''}
                 <button className="logoutBtn" onClick={handleLogout}>Logout</button>
             </div>
         </div>
