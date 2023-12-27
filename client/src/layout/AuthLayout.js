962460b4fc383439fd8fddd2f17c5de232082c29
avinasdube <avidubey712@gamil.com>
2023-12-27 15:35:21 +0530

    [modified] : modified authentication condition for page protection

--- client/src/layout/AuthLayout.js
+++ client/src/layout/AuthLayout.js
@@ -3,28 +3,28 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const Protected = ({ children, authentication = true }) => {
+  const userDetails = useSelector((state) => state.auth.currentUser);
+  // const [authStatus, setAuthStatus] = useState(false);
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
 
-    const authStatus = useSelector((state) => state.auth.status);
+  useEffect(() => {
+    const checkAuthentication = () => {
+      // When authentication is needed but authStatus is not true
+      if (authentication && !userDetails) {
+        navigate('/login');
+      } else if (!authentication && userDetails) {
+        // When authentication is not needed and the user is already logged in
+        navigate('/');
+      }
 
-    const navigate = useNavigate();
+      setLoader(false);
+    };
 
-    const [loader, setLoader] = useState(true);
+    checkAuthentication();
+  }, [userDetails, authentication, navigate]);
 
-    useEffect(() => {
-        // when authentication is needed but authStatus is not true
-        if (authentication && authStatus !== authentication) {
-            navigate('/login');
-        } else if (!authentication && authStatus !== authentication) { // when authentication is not needed and you are already logged in
-            navigate("/")
-        }
-
-        setLoader(false);
-    }, [authStatus, authentication, navigate])
-
-    return loader ?
-        <div>loading</div> :
-        <>{children}</>
-
-}
+  return loader ? <div>Loading...</div> : <>{children}</>;
+};
 
 export default Protected;
