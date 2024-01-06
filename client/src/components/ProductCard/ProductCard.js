3b7e94d090745776404ab2c025a9126e29bc37b5
avinasdube <avidubey712@gamil.com>
2024-01-06 19:19:10 +0530

    [updated] : updated product details rendering logic

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -5,20 +5,25 @@ import heart from '../../assets/icons/favourite.png';
 import cart from '../../assets/icons/cart.png';
 import { Link } from 'react-router-dom';
 
+import { API_BASE_URL, API_ENDPOINTS } from '../../utils/constants';
+
 const ProductCard = ({ product }) => {
     return (
         <div className="cardContainer">
-            <div className="cardImg" style={{ backgroundImage: `url(${product.pImg})` }}>
+            <div className="cardImg"
+                style={{
+                    backgroundImage: `url(${API_BASE_URL}${API_ENDPOINTS.fetchImages}/${product.productId}/${product.images[0]})`
+                }}>
                 <div className="actionImg">
                     <img src={heart} alt=''></img>
                     <img src={cart} alt=''></img>
                 </div>
             </div>
             <div className="cardDetails">
-                <span className="pTitle">{product.title}</span>
+                <span className="pTitle">{product.productName}</span>
                 <div className="checkout">
-                    <span className="price">{product.price}</span>
-                    <Link to={`/products/${product.cat}/${product.title}`} className="buynow">Buy Now</Link>
+                    <span className="price">{product.productPrice}</span>
+                    <Link to={`/products/${product.productCategory}/${product._id}`} className="buynow">Buy Now</Link>
                 </div>
             </div>
         </div>
